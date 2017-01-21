78215a7d86ae1c181dd6bbfd06a29b2c627a1ea8
lefebv-z <marie.lefebvre@epitech.eu>
2017-01-22 00:13:26 +0100

    + movable object + respawn & object

---
+++ waves/Assets/MoveMe.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMe : MonoBehaviour {
+
+    //private bool attachable = false;
+    private bool attached = false;
+    private Vector3 offset;
+    private GameObject owner;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (attached)//a coroutine would be better
+            transform.position = owner.transform.position + offset;
+    }
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            //attachable = true;
+            if (Input.GetButtonDown("Object"))
+            {
+                Debug.Log(collider.gameObject.name);
+                attached = !attached;
+                owner = collider.gameObject;
+                offset = transform.position - owner.transform.position;
+            }
+        }
+    }
+
+}
--- waves/Assets/Scripts/WaveLife.cs
+++ waves/Assets/Scripts/WaveLife.cs
@@ -21,7 +21,7 @@ public class WaveLife : MonoBehaviour {
 
 
     void OnTriggerStay2D(Collider2D other) {
-        Debug.Log(other.transform.position);
+        //Debug.Log(other.transform.position);
     }
     //void WaitAndDestroy()
     //{
