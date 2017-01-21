8f69b6f48b22329dc840040178088e7246681ed6
lefebv-z <marie.lefebvre@epitech.eu>
2017-01-21 17:04:05 +0100

    implement wave

---
+++ waves/Assets/Wave.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour {
+
+    public GameObject wavePrefab;
+    public float cooldownDuration = 1.0f;//seconds
+
+    private float lastWave = -1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time >= lastWave + cooldownDuration && Input.GetButtonDown("Fire1"))
+        {
+            lastWave = Time.time;
+            StartWave();
+        }
+    }
+
+    void StartWave()
+    {
+        Vector3 wavePos = new Vector3(transform.position.x, transform.position.y, 1);
+        Instantiate(wavePrefab, wavePos, Quaternion.identity);
+        //Debug.Log(Input.mousePosition);
+    }
+}
---
+++ waves/Assets/WaveLife.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveLife : MonoBehaviour {
+
+    public float initialRadius = 1.0f;
+    public float propagationSpeed = 1.0f;
+    public float lifetime = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+        transform.localScale = new Vector3(initialRadius, initialRadius, 0);
+        Destroy(gameObject, lifetime);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.localScale += transform.localScale * propagationSpeed * Time.deltaTime;
+	}
+
+    //void WaitAndDestroy()
+    //{
+    //    yield WaitForSeconds(lifetime);
+    //    Destroy(gameObject);
+    //}
+}
