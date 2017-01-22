38d7c1491af12420b7722fd1a9938843d9bab889
Sicarde <camille.koziar@gmail.com>
2017-01-22 13:28:29 +0100

    button feature

---
+++ waves/Assets/Scripts/Button.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+    public Material pressedButtonMaterial;
+    public Sprite pressedButtonSprite;
+    public bool isPressed = false;
+    public GameObject objectToActivate;
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.collider.gameObject.layer == LayerMask.NameToLayer("player") ||
+            other.collider.gameObject.layer == LayerMask.NameToLayer("object")) {
+            if (other.collider.transform.position.y > transform.position.y) {
+                isPressed = true;
+                GetComponent<SpriteRenderer>().material = pressedButtonMaterial;
+                GetComponent<SpriteRenderer>().sprite = pressedButtonSprite;
+                objectToActivate.SetActive(true);
+            }
+        }
+    }
+}
--- waves/Assets/Scripts/WaveLife.cs
+++ waves/Assets/Scripts/WaveLife.cs
@@ -8,6 +8,8 @@ public class WaveLife : MonoBehaviour {
     public float propagationSpeed = 1.0f;
     public float lifetime = 2.0f;
     public Material wallMaterial;
+    public Material buttonMaterial;
+    public Material activatedButtonMaterial;
     private float _nbFrames = 0.0f;
 
     // Use this for initialization
@@ -20,15 +22,20 @@ public class WaveLife : MonoBehaviour {
 	void FixedUpdate () {
         transform.localScale += transform.localScale * propagationSpeed * Time.deltaTime;
         wallMaterial.SetFloat("_WaveSize", transform.localScale.x);
+        buttonMaterial.SetFloat("_WaveSize", transform.localScale.x);
+        activatedButtonMaterial.SetFloat("_WaveSize", transform.localScale.x);
         if (_nbFrames == 0.0f) {
             wallMaterial.SetVector("_WaveOrigin", new Vector4(transform.position.x, transform.position.y, transform.position.z, 0.0f));
+            buttonMaterial.SetVector("_WaveOrigin", new Vector4(transform.position.x, transform.position.y, transform.position.z, 0.0f));
+            activatedButtonMaterial.SetVector("_WaveOrigin", new Vector4(transform.position.x, transform.position.y, transform.position.z, 0.0f));
         }
         _nbFrames++;
     }
 
     void OnDestroy() {
         wallMaterial.SetFloat("_WaveSize", 0);
-        Debug.Log(0);
+        buttonMaterial.SetFloat("_WaveSize", 0);
+        activatedButtonMaterial.SetFloat("_WaveSize", 0);
     }
 
     //void WaitAndDestroy()
