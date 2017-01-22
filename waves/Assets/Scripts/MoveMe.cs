e5ffa97d9773626a5294089d7c87176d993ad335
lefebv-z <marie.lefebvre@epitech.eu>
2017-01-22 05:18:33 +0100

    ambiant sound & box sound effect

--- waves/Assets/Scripts/MoveMe.cs
+++ waves/Assets/Scripts/MoveMe.cs
@@ -8,10 +8,11 @@ public class MoveMe : MonoBehaviour {
     private bool attached = false;
     private Vector3 offset;
     private GameObject owner;
+    private AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
-		
+        audioSource = gameObject.GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -31,6 +32,8 @@ public class MoveMe : MonoBehaviour {
                 attached = !attached;
                 owner = collider.gameObject;
                 offset = transform.position - owner.transform.position;
+                if (attached)
+                    audioSource.Play();
             }
         }
     }
